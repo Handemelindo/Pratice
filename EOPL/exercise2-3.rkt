a1855b5c973d8e2a136a483e1b70229529de2b7b
Handemelindo <yuanhangwang.silvio@gmail.com>
2015-04-27 13:17:05 +0800

    add 2.30 & 2.31

--- EOPL/exercise2-3.rkt
+++ EOPL/exercise2-3.rkt
@@ -1,5 +1,6 @@
 #lang eopl
 (require rackunit)
+(require racket/format)
 
 (define list-of
   (lambda (pred)
@@ -8,7 +9,7 @@
           (and (pair? val)
                (pred (car val))
                (list-of pred) (cdr val))))))
-;;2.29[*]
+;;2.29[*] 2.30[**]
 (define-datatype lc-exp lc-exp?
   (var-exp
    (var symbol?))
@@ -24,11 +25,18 @@
     (cond
      ((symbol? exp) (var-exp exp))
      ((pair? exp)
-      (if (eqv? (car exp) 'lambda)
-          (lambda-exp (cadr exp) (parse-expression (caddr exp)))
-          (app-exp
+      (cond
+       ((eqv? (car exp) 'lambda)
+        (if (= (length exp) 3)
+            (lambda-exp (cadr exp) (parse-expression (caddr exp)))
+            (eopl:error 'lambda-expression-should-contain-three-parts (~a exp))))
+       ((> (length exp) 1)
+        (app-exp
            (parse-expression (car exp))
-           (map parse-expression (cdr exp))))))))
+           (map parse-expression (cdr exp))))
+       (else
+        (eopl:error 'invalid-lambda-expression-of-list (~a exp)))))
+     (else (eopl:error 'invalid-lambda-expression (~a exp))))))
 
 (define lc-exp-unparse
   (lambda (exp)
@@ -51,4 +59,38 @@
   (check-equal? (lc-exp-unparse (parse-expression lambda1-expr)) lambda1-expr)
   (check-equal? (lc-exp-unparse (parse-expression app1-expr)) app1-expr))
 
-;;2.30[**]
+;;2.31[**]
+(define-datatype prefix-exp prefix-exp?
+  (const-exp
+   (num integer?))
+  (diff-exp
+   (minuend prefix-exp?)
+   (subtrahend prefix-exp?)))
+
+(define polish-parse
+  (lambda (tokens)
+    (cond
+     ((eqv? (car tokens) '-)
+      (letrec
+          ((r1 (polish-parse (cdr tokens)))
+           (fst (car r1))
+           (rest1 (cadr r1))
+           (r2 (polish-parse rest1))
+           (snd (car r2))
+           (rest2 (cadr r2)))
+           (list (diff-exp fst snd) rest2)))
+     ((integer? (car tokens))
+      (list (const-exp (car tokens)) (cdr tokens)))
+     (else
+      (eopl:error 'invalid-symbol)))))
+
+(check-equal? (car (polish-parse '(- - 3 2 - 4 - 12 7)))
+              (diff-exp
+               (diff-exp
+                (const-exp 3)
+                (const-exp 2))
+               (diff-exp
+                (const-exp 4)
+                (diff-exp
+                 (const-exp 12)
+                 (const-exp 7)))))
