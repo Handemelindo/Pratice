aeaebda093afa2c19bb03d0a7e3aabde54df7ca0
Handemelindo <yuanhangwang.silvio@gmail.com>
2015-07-22 13:19:08 +0800

    add 3.7 of EOPL.

--- EOPL/exercise3-let.rkt
+++ EOPL/exercise3-let.rkt
@@ -30,6 +30,12 @@
     (expression
      ("-" "(" expression "," expression ")") diff-exp)
     (expression
+     ("+" "(" expression "," expression ")") plus-exp)
+    (expression
+     ("*" "(" expression "," expression ")") mul-exp)
+    (expression
+     ("/" "(" expression "," expression ")") div-exp)
+    (expression
      ("zero?(" expression ")") zero?-exp)
     (expression
      ("minus(" expression ")") minus-exp)
@@ -68,37 +74,50 @@
   (lambda (pgm env)
     (value-of pgm env)))
 
+(define value-of->num
+  (lambda (exp env)
+    (expval->num (value-of exp env))))
+(define value-of->bool
+  (lambda (exp env)
+    (expval->bool (value-of exp env))))
+(define lift-num
+  (lambda (f env . exprs)
+    (num-val (apply
+              f
+              (map
+               (lambda (expr) (value-of->num expr env))
+               exprs)))))
 (define value-of
   (lambda (exp env)
     (cases expression exp
            (const-exp (num) (num-val num))
            (var-exp (var) (apply-env var env))
            (diff-exp (expr1 expr2)
-                     (let ((val1 (value-of expr1 env))
-                           (val2 (value-of expr2 env)))
-                       (let ((num1 (expval->num val1))
-                             (num2 (expval->num val2)))
-                         (num-val (- num1 num2)))))
+                     (lift-num - env expr1 expr2))
+           (plus-exp (expr1 expr2)
+                     (lift-num + env expr1 expr2))
+           (div-exp (expr1 expr2)
+                     (lift-num / env expr1 expr2))
+           (mul-exp (expr1 expr2)
+                     (lift-num * env expr1 expr2))
            (zero?-exp (expr)
-                      (let ((val (value-of expr env)))
-                        (let ((num (expval->num val)))
-                          (if (zero? num)
-                              (bool-val #t)
-                              (bool-val #f)))))
+                      (let ((num (value-of->num expr env)))
+                        (if (zero? num)
+                            (bool-val #t)
+                            (bool-val #f))))
            (minus-exp (expr)
-                      (let ((val (value-of expr env)))
-                        (let ((num (expval->num val)))
-                          (num-val (- num)))))
+                      (let ((num (value-of->num expr env)))
+                        (num-val (- num))))
            (if-exp (predicate if-exp false-exp)
-                   (let ((val (value-of predicate env)))
-                     (let ((pred (expval->bool val)))
-                       (if pred
-                           (value-of if-exp env)
-                           (value-of false-exp env)))))
+                   (let ((pred (value-of->bool predicate env)))
+                     (if pred
+                         (value-of if-exp env)
+                         (value-of false-exp env))))
            (let-exp (var val-exp body)
                     (let ((val (value-of val-exp env)))
                       (let ((new-env (extend-env var val env)))
                         (value-of body new-env)))))))
+
 (check-equal? (expval->num
                (run "-(-(x, 3), -(v, i))"
                     (extend-env 'i (num-val 1)
@@ -106,6 +125,14 @@
                       (extend-env 'x (num-val 10)
                                   (empty-env))))))
               3)
+
+(check-equal? (expval->num
+               (run "/(-(*(x, 3), +(v, i)), 7)"
+                    (extend-env 'i (num-val 1)
+                     (extend-env 'v (num-val 5)
+                      (extend-env 'x (num-val 10)
+                                  (empty-env))))))
+              (/ 24 7))
 
 (check-equal? (expval->num
                (run "if zero?(-(x, 11)) then -(y, 2) else -(y, 4)"
